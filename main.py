b8916e6c13bb51db825c54ec99908baf63458689
Priyanshu R <priyanshuranjan7004@GMAIL.COM>
2026-01-20 21:54:09 +0530

    Upgrading it

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-import tkinter
+import customtkinter as ct
 
 
 from tkinter import ttk
 
 
-screen = tkinter.Tk()
-screen.title("PASSWORD MANAGER")
-
-
-
+screen = ct.CTk()
+# screen.title("PASSWORD MANAGER")
 screen.geometry("800x600")
 
 
 
-
+button= ct.CTkButton(screen,text="My_Button",fg_color="red")
+button.place(x=100,y=200)
 
 
 
 screen.mainloop()
+

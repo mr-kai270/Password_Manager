64bd5019415fc4a098a3d6d6b0533dd1e955a9b3
Priyanshu R <priyanshuranjan7004@GMAIL.COM>
2026-01-20 19:40:15 +0530

    Basic Frame

---
+++ eamil.py
@@ -0,0 +1,18 @@
+import smtplib
+
+
+def send_eamil(cod,to):
+
+
+    my_eamil = "priyanshuranjan7004@gmail.com"
+    password = "wapcmcrqczzxymxn"
+
+
+    connections = smtplib.SMTP("smtp.gmail.com")
+    connections.starttls()
+    connections.login(user=my_eamil, password=password)
+    connections.sendmail(from_addr=my_eamil, to_addrs=to
+                         , msg=cod)
+    connections.close()
+
+
--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 import tkinter
-import smtplib
 
 
 from tkinter import ttk
 
-my_eamil = "priyanshuranjan7004@gmail.com"
-password = "wapcmcrqczzxymxn"
 
-
-connections = smtplib.SMTP("smtp.gmail.com")
-connections.starttls()
-connections.login(user=my_eamil, password=password)
-connections.sendmail(from_addr=my_eamil,to_addrs="priyanshuranjan2700@gmail.com"
-                     ,msg="Hello")
-connections.close()
+screen = tkinter.Tk()
+screen.title("PASSWORD MANAGER")
